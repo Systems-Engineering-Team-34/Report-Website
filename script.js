508d380f8d0253a132d1ae0c9e8f1cb8727c9fe9
Poom Kijroan <pittawas.kijoran@gmail.com>
2024-03-08 15:00:43 +0000

    Added bootstrap template

--- script.js
+++
@@ -1,9 +0,0 @@
-const toTop = document.getElementById('to_top_button');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 100) {
-    toTop.style.display = 'block';
-  } else {
-    toTop.style.display = 'none';
-  }
-});
